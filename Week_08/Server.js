9aa58c801a23162f36a00573f6319b3c848627fa
Tongzhehui98 <tongzhehui1993@qq.com>
2020-11-08 20:01:21 +0800

    完成第十周作业

--- Week_08/Server.js
+++ Week_08/Server.js
@@ -2,15 +2,15 @@ const http = require('http');
 
 console.log(http)
 http.createServer((request, response) => {
-    let body = [];
-    request.on("error", err => {
-        console.log(err)
-    }).on('data', chunk => {
-        body.push(chunk.toString())
-    }).on('end', () => {
-        body = Buffer.concat(body).toString();
-        response.writeHead(200, {'Content-Type':'text/html'});
-        response.end(`
+  let body = [];
+  request.on("error", err => {
+    console.log(err)
+  }).on('data', chunk => {
+    body.push(chunk.toString())
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(`
           <html mass=a >
             
             <head>
@@ -28,5 +28,7 @@ http.createServer((request, response) => {
             </body>
           </html>
         `);
-    })
+  })
 }).listen(3001)
+
+
